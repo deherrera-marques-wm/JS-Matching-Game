2fe6bf57815bf07b2136243b4bf08366d5a973b8
Marques DeHerrera <session1@START-Coding-MBP-08.local>
2016-08-15 15:21:17 -0700

    Part 2 of the javascript matching game

--- matchgame.js
+++ matchgame.js
@@ -1,1 +1,11 @@
-
+function generateFaces() {
+    var numberOfFaces = 5;
+    var theLeftSide = document.getElementById("leftSide");
+    for (i = 0; i < numberOfFaces; ++ i) {
+        pokeball = document.createElement("img");
+        pokeball.src = "pokeball.png";
+        pokeball.style.top = Math.floor(Math.random() * 401) + "px";
+        pokeball.style.left = Math.floor(Math.random() * 401) + "px";
+        theLeftSide.appendChild(pokeball);
+    }
+}
